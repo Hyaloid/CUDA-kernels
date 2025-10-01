2ff42b263a6f49b66f79a3cc54b42a6b1887ceca
Hyaloid <hyaloids@163.com>
2025-10-01 15:24:20 +0000

    add annotations and support cross blocks prefix sum in block_reduce_kernel

--- kernels/block_reduce/block_reduce_kernel.cu
+++ kernels/block_reduce/block_reduce_kernel.cu
@@ -1,34 +1,57 @@
 #include <torch/extension.h>
 #include <cuda_runtime.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <cooperative_groups.h>
+
+__device__ __host__ inline
+bool isPowerOfTwo(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
 
 template <typename T>
 __global__
 void inclusivePrefixSumKernel(const T* input, T* output, int n) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    namespace cg = cooperative_groups;
+    auto block = cg::this_thread_block();
+    int offset = blockIdx.x * blockDim.x;
+    int tid = threadIdx.x;
+    int block_len = min(n - offset, blockDim.x);
     
-    if (tid >= n) return;
+    if (tid >= block_len) return;
+
+    extern __shared__ int block_sum[];
+    T* shared_block_sum = reinterpret_cast<T*>(block_sum);
     
-    output[tid] = input[tid];
-    
+    output[tid + offset] = input[tid + offset];
     __syncthreads();
     
     // up-sweep
-    for (int stride = 1; stride < n; stride *= 2) {
+    for (int stride = 1; stride < block_len; stride <<= 1) {
         int index = (tid + 1) * stride * 2 - 1;
-        if (index < n) {
-            output[index] += output[index - stride];
+        if (index < block_len) {
+            output[index + offset] += output[index - stride + offset];
         }
         __syncthreads();
     }
     
     // down-sweep
-    for (int stride = n / 2; stride > 0; stride /= 2) {
+    for (int stride = block_len / 2; stride > 0; stride >>= 1) {
         int index = (tid + 1) * stride * 2 - 1;
-        if (index + stride < n) {
-            output[index + stride] += output[index];
+        if (index + stride < block_len) {
+            output[index + stride + offset] += output[index + offset];
         }
         __syncthreads();
+    }
+    // block sync
+    block.sync();
+    if (tid < gridDim.x) {
+        shared_block_sum[tid] = output[(tid + 1) * blockDim.x - 1];
+    }
+    __syncthreads();
+
+    #pragma unroll
+    for (int i = 0; i < blockIdx.x; ++i) {
+        output[tid + offset] += shared_block_sum[i];
     }
 }
 
@@ -36,7 +59,14 @@ torch::Tensor inclusive_prefix_sum(
     const torch::Tensor& input,
     torch::Tensor& output
 ) {
+    /*
+        This algorithm requires the input_len to be a power of two.
+        input: [1, 2, 3, 4, 5, 6]
+        output: [1, 3, 6, 10, 15, 21]
+    */
     const int input_len = input.size(0);
+
+    TORCH_CHECK(isPowerOfTwo(input_len), "input_len must be a power of two, but got ", input_len);
 
     constexpr int block_size = 128;
     const int grid_size = (input_len + block_size - 1) / block_size;
@@ -45,12 +75,13 @@ torch::Tensor inclusive_prefix_sum(
     const dim3 block(block_size);
 
     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+    int shared_mem_size = grid_size * sizeof(int);
     
     AT_DISPATCH_ALL_TYPES(
         input.scalar_type(),
         "inclusivePrefixSum",
         [&] {
-            inclusivePrefixSumKernel<scalar_t><<<grid, block, 0, stream>>>(
+            inclusivePrefixSumKernel<scalar_t><<<grid, block, shared_mem_size, stream>>>(
                 input.data_ptr<scalar_t>(),
                 output.data_ptr<scalar_t>(),
                 input_len
--- kernels/block_reduce/setup.py
+++ kernels/block_reduce/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 sys.path.append(str(Path(__file__).parent.parent.parent))
-from utils import CXX_FLAGS, NVCC_FLAGS, CUDA_INCLUDES, LINK_FLAGS
+from utils import CXX_FLAGS, NVCC_FLAGS, CUDA_INCLUDES, LINK_FLAGS, CUDA_LIBS
 
 setup(
     name='block_reduce',
@@ -13,6 +13,7 @@ setup(
             name='block_reduce',
             sources=['block_reduce_kernel.cu', 'block_reduce.cpp'],
             include_dirs=CUDA_INCLUDES,
+            library_dirs=CUDA_LIBS,
             extra_compile_args={
                 'cxx': CXX_FLAGS,
                 'nvcc': NVCC_FLAGS,
